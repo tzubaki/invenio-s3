2b6a16d0249af534146fd52ea17d033326c42988
Parth Shandilya <parth.shandilya@cern.ch>
2020-12-07 10:16:01 +0100

    global: migrate CI to gh-actions
    closes #21

--- setup.py
+++ setup.py
@@ -14,25 +14,17 @@ readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
 tests_require = [
-    'check-manifest>=0.25',
-    'coverage>=5.0',
+    'pytest-invenio>=1.4.0',
     'invenio-base>=1.2.2',
     'invenio-app>=1.2.3',
     'invenio-db[all]>=1.0.4',
-    'isort>=4.3.3',
     'moto>=1.3.7',
-    'pydocstyle>=1.0.0',
-    'pytest-cache>=1.0',
-    'pytest-cov>=1.8.0',
-    'pytest-invenio>=1.3.0',
-    'pytest-pep8>=1.0.6',
-    'pytest>=4.6.4,<5.0.0',
     'redis>=2.10.5',
 ]
 
 extras_require = {
     'docs': [
-        'Sphinx>=1.5.1,<3.0.2',
+        'Sphinx>=3,<3.0.2',
     ],
     'tests': tests_require,
 }
